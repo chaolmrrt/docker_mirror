39654147c1ebe851941021af49d89655200aa370
LeMeng Chao(朝乐蒙) <lmchao@hillstonenet.com>
2021-09-09 09:42:50 +0800

    update

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]; then
 fi
 
 for x in `cat $1`; do
-	_dir=`echo $x| sed 's,/,#,g'| awk -F':' '{print $1}'`
+	_dir=`echo $x| sed 's,/,__,g'| awk -F':' '{print $1}'`
 	_ver=`echo $x| awk -F':' '{print $2}'`
 	if [ -z "$_ver" ]; then
 		_ver="null"
