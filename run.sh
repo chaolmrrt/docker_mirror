dd9cab17c3908c8823e94040e6b17831c225647a
LeMeng Chao(朝乐蒙) <lmchao@hillstonenet.com>
2021-09-09 09:22:01 +0800

    update

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+
+if [ -z "$1" ]; then
+	exit 1
+fi
+
+for x in `cat $1`; do
+	_dir=`echo $x| sed 's,/,#,g'| awk -F':' '{print $1}'`
+	_ver=`echo $x| awk -F':' '{print $2}'`
+	if [ -z "$_ver" ]; then
+		_ver="null"
+	fi
+	mkdir -p $_dir
+	echo "FROM $x" > $_dir/$_ver
+done
