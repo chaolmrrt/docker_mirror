248745f964ec01aa29411c051e61a4ca89c52d29
LeMeng Chao(朝乐蒙) <lmchao@hillstonenet.com>
2021-12-16 12:59:22 +0800

    update

--- import_img.sh
+++ import_img.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-	echo './import_img.sh  ceph_ceph:v16'
+if [ $# -lt 1 ]; then
+	echo './import_img.sh  ceph_ceph:v16 [path]'
 	exit
 fi
 
 _mirror="registry.cn-beijing.aliyuncs.com/rrt"
-_upload="127.0.0.1/pub"
+if [ -n "$2" ]; then
+	_upload="127.0.0.1/$2"
+else
+	_upload="127.0.0.1/pub"
+fi
 
 
 _url_from_mirror=$_mirror"/"`echo $1|sed 's,/,_,g'`
