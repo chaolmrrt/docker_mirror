2d3f3d8c10c33483ff3be0f85b17ee1bada65628
rrt <13911023980@139.com>
2022-07-19 13:15:10 +0800

    update

--- wazuh/rpm_builder_aarch64/v4.3.0/i.sh
+++ wazuh/rpm_builder_aarch64/v4.3.0/i.sh
@@ -1,16 +1,8 @@
-FROM arm64v8/centos:7
+#!/bin/bash -e
 
-RUN sed -e 's|^mirrorlist=|#mirrorlist=|g' \
-         -e 's|^#baseurl=http://mirror.centos.org/altarch/|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-altarch/|g' \
-         -e 's|^#baseurl=http://mirror.centos.org/$contentdir/|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-altarch/|g' \
-         -i.bak \
-         /etc/yum.repos.d/CentOS-*.repo
+yum install -y http://packages.wazuh.com/utils/pkg/epel-release-latest-7.noarch.rpm
 
-# Enable EPEL
-RUN yum install -y http://packages.wazuh.com/utils/pkg/epel-release-latest-7.noarch.rpm
-
-# Install all the necessary tools to build the packages
-RUN yum install -y gcc make wget git \
+yum install -y gcc make wget git \
     openssh-clients sudo gnupg file-devel\
     automake autoconf libtool policycoreutils-python \
     yum-utils system-rpm-config rpm-devel \
@@ -30,7 +22,7 @@ RUN yum install -y gcc make wget git \
     redhat-rpm-config sqlite-devel gdb tar tcl-devel tix-devel tk-devel \
     valgrind-devel python-rpm-macros python34 nodejs
 
-RUN curl -OL http://packages.wazuh.com/utils/gcc/gcc-9.4.0.tar.gz && \
+curl -OL http://packages.wazuh.com/utils/gcc/gcc-9.4.0.tar.gz && \
     tar xzf gcc-9.4.0.tar.gz  && cd gcc-9.4.0/ && \
     ./contrib/download_prerequisites && \
     ./configure --prefix=/usr/local/gcc-9.4.0 --enable-languages=c,c++ --disable-multilib \
@@ -38,38 +30,37 @@ RUN curl -OL http://packages.wazuh.com/utils/gcc/gcc-9.4.0.tar.gz && \
     make -j$(nproc) && make install && \
     ln -fs /usr/local/gcc-9.4.0/bin/g++ /usr/bin/c++ && cd / && rm -rf gcc-*
 
-ENV CPLUS_INCLUDE_PATH "/usr/local/gcc-9.4.0/include/c++/9.4.0/"
-ENV LD_LIBRARY_PATH "/usr/local/gcc-9.4.0/lib64/"
-ENV PATH "/usr/local/gcc-9.4.0/bin:${PATH}"
+export CPLUS_INCLUDE_PATH="/usr/local/gcc-9.4.0/include/c++/9.4.0/"
+export LD_LIBRARY_PATH="/usr/local/gcc-9.4.0/lib64/"
+export PATH="/usr/local/gcc-9.4.0/bin:${PATH}"
 
-RUN curl -OL http://packages.wazuh.com/utils/cmake/cmake-3.18.3.tar.gz && \
+curl -OL http://packages.wazuh.com/utils/cmake/cmake-3.18.3.tar.gz && \
     tar -zxf cmake-3.18.3.tar.gz && cd cmake-3.18.3 && \
     ./bootstrap --no-system-curl CC=/usr/local/gcc-9.4.0/bin/gcc \
         CXX=/usr/local/gcc-9.4.0/bin/g++ && \
     make -j$(nproc) && make install && cd / && rm -rf cmake-*
 
-RUN curl -O http://packages.wazuh.com/utils/openssl/openssl-1.1.1a.tar.gz && \
+curl -O http://packages.wazuh.com/utils/openssl/openssl-1.1.1a.tar.gz && \
     tar -xzf openssl-1.1.1a.tar.gz && cd openssl* && \
     ./config -Wl,--enable-new-dtags,-rpath,'$(LIBRPATH)' && \
     make -j $(nproc) && make install && cd / && rm -rf openssl-*
 
-RUN curl -O http://packages.wazuh.com/utils/nodejs/node-v12.16.1-linux-arm64.tar.xz && \
+curl -O http://packages.wazuh.com/utils/nodejs/node-v12.16.1-linux-arm64.tar.xz && \
     tar -xJf node-v12.16.1-linux-arm64.tar.xz && \
     cd node-v12.16* && cp -R * /usr/local/ && cd / && rm -rf node-v*
 
 # Update rpmbuild, rpm and autoconf
-RUN curl -O http://packages.wazuh.com/utils/autoconf/autoconf-2.69.tar.gz && \
+curl -O http://packages.wazuh.com/utils/autoconf/autoconf-2.69.tar.gz && \
     gunzip autoconf-2.69.tar.gz && tar xvf autoconf-2.69.tar && \
     cd autoconf-2.69 && ./configure && \
     make -j $(nproc) && make install && cd / && rm -rf autoconf-*
 
-RUN curl -O http://packages.wazuh.com/utils/rpm/rpm-4.15.1.tar.bz2 && \
+curl -O http://packages.wazuh.com/utils/rpm/rpm-4.15.1.tar.bz2 && \
     tar -xjf rpm-4.15.1.tar.bz2 && cd rpm-4.15.1 && \
     ./configure --without-lua && make -j $(nproc) && \
     make install && cd / && rm -rf rpm-*
 
-RUN mkdir -p /usr/local/var/lib/rpm && \
+mkdir -p /usr/local/var/lib/rpm && \
     cp /var/lib/rpm/Packages /usr/local/var/lib/rpm/Packages && \
     /usr/local/bin/rpm --rebuilddb && rm -rf /root/rpmbuild
 
-CMD ["/bin/bash"]
--- wazuh/run.sh
+++ wazuh/run.sh
@@ -4,6 +4,12 @@ t=`basename $p`
 p=`dirname $p`
 p=`basename $p`
 
+if [ -n "$1" ]; then
+	ip="$1"
+else
+	ip="127.0.0.1"
+fi
+
 echo $p:$t
 docker build  --network=host -f Dockerfile -t "$p:$t" --add-host=gcc.gnu.org:127.0.0.1 --add-host=packages.wazuh.com:127.0.0.1 .
 echo $p:$t
